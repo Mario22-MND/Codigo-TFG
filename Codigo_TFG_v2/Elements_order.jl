a9e17e9b3955c8a38f664725abc4ded773ac049a
Mario22-MND <126000794+Mario22-MND@users.noreply.github.com>
2024-01-28 16:36:34 +0100

    Delete Codigo_TFG_v2 directory

--- Codigo_TFG_v2/Elements_order.jl
+++
@@ -1,9 +0,0 @@
-module Elements_order
-
-global GMSH_TRIA_ORDER_1 = 2
-global GMSH_TRIA_ORDER_2 = 9
-global GMSH_TRIA_ORDER_3 = 21
-global GMSH_QUAD_ORDER_1 = 3
-global GMSH_QUAD_ORDER_2 = 10
-
-end
