7da05aea67eb7b21eabea6c139e48d26af693cbb
Mario22-MND <126000794+Mario22-MND@users.noreply.github.com>
2024-02-03 09:54:33 +0100

    Update Boundary_cond.jl

--- Boundary_cond.jl
+++ Boundary_cond.jl
@@ -16,13 +16,11 @@ struct Boundary_condition
             descript = "Son esenciales y deben fijarse explícitamente"
         elseif type == NEUMAN
             descript = "Son naturales y se aplican implícitamente"
-        elseif type == CAUCHY
-            descript = ""
         end
         nodes = [Int64(x) for x in nodes]
         id = convert(Int64, id)
         new(id, descript, type, nodes)
      end
 end
-
+            
 end
